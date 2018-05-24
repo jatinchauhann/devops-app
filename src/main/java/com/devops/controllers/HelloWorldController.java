a5aada5d6210bf68a26427dd44a4ba59e833186b
JatinChauhan1998 <jtnchauhan24@gmail.com>
2018-05-24 11:31:09 +0530

    Added the controller

---
+++ src/main/java/com/devops/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.devops.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello World!";
+	}
+}
