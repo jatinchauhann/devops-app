4b67cac03d3f9b5567059122a9f5d692bee4584f
JatinChauhan1998 <jtnchauhan24@gmail.com>
2018-05-24 19:17:00 +0530

    Added files to repo

--- src/main/java/com/devops/DevopsApplication.java
+++ src/main/java/com/devops/DevopsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsApplication.class, args);
+    }
 }
--- src/main/java/com/devops/controllers/HelloWorldController.java
+++ src/main/java/com/devops/controllers/HelloWorldController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
-	
-	@RequestMapping("/")
-	public String sayHello() {
-		return "index";
-	}
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "index";
+    }
 }
