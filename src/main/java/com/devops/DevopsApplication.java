bc966068cfbaa4ebab2a1064bbbc940d350d9caa
JatinChauhan1998 <jtnchauhan24@gmail.com>
2018-05-24 11:12:28 +0530

    Initial commit (24MAY)

---
+++ src/main/java/com/devops/DevopsApplication.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApplication.class, args);
+	}
+}
